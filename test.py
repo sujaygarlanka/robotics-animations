d6e11a9299c6afe5324b9a54ec1a9b34f2d0238b
Sujay Garlanka <sujay.garlanka@gmail.com>
2025-05-22 17:53:11 -0400

    syncing remote desktop

--- test.py
+++ test.py
@@ -265,7 +265,7 @@ class TorusPlot(InteractiveScene):
         # Create and animate a large centered copy of the first cube
         large_cube = cubes[0].copy()
         self.play(
-            large_cube.animate.scale(3).move_to(ORIGIN),
+            large_cube.animate.scale(5).move_to(ORIGIN),
             run_time=1.5
         )
 
