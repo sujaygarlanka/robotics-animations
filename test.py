0eee4d217a80f0180ceb94981ea87dd9d6defa75
Sujay Garlanka <sujay.garlanka@gmail.com>
2025-05-22 17:02:30 -0400

    syncing remote desktop

--- test.py
+++ test.py
@@ -229,7 +229,7 @@ class TorusPlot(InteractiveScene):
         self.play(*rotations, run_time=2)
 
         # Mirror the cubes
-        for cube in cubes:
+        # for cube in cubes:
 
         # Create target arrangement
         target_group = Group(*cubes).copy()
