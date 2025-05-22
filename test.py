b6020b4831fad49890f3bc13fd65a169ff0b4671
Sujay Garlanka <sujay.garlanka@gmail.com>
2025-05-22 17:19:05 -0400

    syncing remote desktop

--- test.py
+++ test.py
@@ -79,8 +79,8 @@ class Cube(Group):
             return vertices
         else:
             for dot in self.vertices:
-                self.scene.remove(dot)
                 self.remove(dot)
+                self.scene.remove(dot)
             self.vertices = []
             self.vertex_idx = []
 
