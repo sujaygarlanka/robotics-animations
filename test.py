a41139ff720bf3b7c1839ebdf8e50b4d78300052
Sujay Garlanka <sujay.garlanka@gmail.com>
2025-05-23 09:29:21 -0400

    syncing remote desktop

--- test.py
+++ test.py
@@ -278,10 +278,6 @@ class TorusPlot(InteractiveScene):
         )
         large_cube._scale(5)
 
-        # embed()
-
-        # print(large_cube.scale_val)
-        # embed()
         # Show mirroring
         mirror_idx = {0, 1, 2, 3, 4, 5, 6, 7} - set(large_cube.vertex_idx)
         v = large_cube.clear_vertices(animation=True)
