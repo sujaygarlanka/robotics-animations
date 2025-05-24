f24b4f6f022e49165a06d518fdf8a346d41c62ef
Sujay Garlanka <sujay.garlanka@gmail.com>
2025-05-24 12:34:31 -0400

    syncing remote desktop

--- test.py
+++ test.py
@@ -146,7 +146,7 @@ class Cube(Group):
             scale=self.scale_val,
         )
         new_cube.rotation_matrix = self.rotation_matrix.copy()
-        print(self.vertex_idx)
+        print(new_cube.rotation_matrix)
         new_cube.add_vertices(self.vertex_idx)
         new_cube.add_triangles(self.triangles_idx)
         new_cube.move_to(self.get_center())
