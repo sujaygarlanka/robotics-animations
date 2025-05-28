66511813f88aba660cc08f3d96b14a914eaea0d3
Sujay Garlanka <sujay.garlanka@gmail.com>
2025-05-28 14:50:14 -0400

    Reorganize code

--- animations/utils.py
+++ animations/utils.py
@@ -44,3 +44,49 @@ self.add(coord_frame)
 # Make the text labels face the camera
 for label in [coord_frame[3], coord_frame[4], coord_frame[5]]:  # The text labels
     self.add_fixed_orientation_mobjects(label)
+
+
+############################################################################
+def create_coordinate_frame(self, origin=ORIGIN, axis_length=1.0, axis_thickness=0.05):
+    """Create a 3D coordinate frame with X, Y, and Z axes."""
+    x_axis = Cylinder(
+        radius=axis_thickness,
+        height=axis_length,
+        axis=np.array([axis_length, 0, 0]),
+        color=RED,
+    )
+    
+    y_axis = Cylinder(
+        radius=axis_thickness,
+        height=axis_length,
+        axis=np.array([0, axis_length, 0]),
+        color=GREEN,
+    )
+    
+    z_axis = Cylinder(
+        radius=axis_thickness,
+        height=axis_length,
+        axis=np.array([0, 0, axis_length]),
+        color=BLUE,
+    )
+    
+    # Labels
+    x_label = Text("X", color=RED).scale(0.5).next_to(x_axis.get_end(), RIGHT, buff=0.1)
+    y_label = Text("Y", color=GREEN).scale(0.5).next_to(y_axis.get_end(), UP, buff=0.1)
+    z_label = Text("Z", color=BLUE).scale(0.5).next_to(z_axis.get_end(), OUT, buff=0.1)
+    
+    # Group all elements
+    frame = Group(x_axis, y_axis, z_axis, x_label, y_label, z_label)
+    return frame
+    
+# Add a coordinate frame to the scene
+coord_frame = self.create_coordinate_frame(
+    origin=np.array([-4, -3, -3]),  # Position in bottom left corner
+    axis_length=1.5,
+    axis_thickness=0.05
+)
+self.add(coord_frame)
+
+# Make the text labels face the camera
+for label in [coord_frame[3], coord_frame[4], coord_frame[5]]:  # The text labels
+    self.add(label)
